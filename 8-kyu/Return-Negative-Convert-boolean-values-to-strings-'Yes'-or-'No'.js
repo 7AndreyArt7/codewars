4f2266bd3c2a357bbe52e0aa1446a325d8c4803a
7AndreyArt7 <7andron7art@gmail.com>
2025-04-29 20:14:25 +0300

    Return-Negative-Convert-boolean-values-to-strings-'Yes'-or-'No'

--- 8-kyu/Return-Negative-Convert-boolean-values-to-strings-'Yes'-or-'No'.js
+++ 8-kyu/Return-Negative-Convert-boolean-values-to-strings-'Yes'-or-'No'.js
@@ -1,12 +1,13 @@
 // Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
 
 
-function boolToWord( bool ){
-    if (  bool === true){
-       return  bool= "Yes";
-    }{
-      return   bool= "No";
+function boolToWord(bool) {
+    if (bool) {
+        return bool = "Yes";
+    }
+    {
+        return bool = "No";
     }
 }
 
-console.log( boolToWord( false) );
+console.log(boolToWord(true));
